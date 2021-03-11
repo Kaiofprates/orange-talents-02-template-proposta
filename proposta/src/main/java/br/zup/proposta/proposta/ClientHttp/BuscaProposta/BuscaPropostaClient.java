7b6e44ec29098b1777284cd701e54e5eb31387af
Kaiofprates <kaioprates@outlook.com>
2021-03-11 01:57:37 -0300

    [testes] adiciona variáveis de ambiente

--- proposta/src/main/java/br/zup/proposta/proposta/ClientHttp/BuscaProposta/BuscaPropostaClient.java
+++ proposta/src/main/java/br/zup/proposta/proposta/ClientHttp/BuscaProposta/BuscaPropostaClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(url = "http://localhost:8888/api", name = "buscaProposta")
+@FeignClient(url = "${api.proposta}", name = "buscaProposta")
 public interface BuscaPropostaClient {
 
     @GetMapping("/cartoes")

dd6a12a28526e0c806d0b010545da766ef011dad
Kaiofprates <kaioprates@outlook.com>
2021-03-14 21:37:12 -0300

    [environment] organizando profiles

--- proposta/src/test/java/br/zup/proposta/proposta/CartaoTestes.java
+++ proposta/src/test/java/br/zup/proposta/proposta/CartaoTestes.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -32,6 +33,7 @@ import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Profile("dev")
 public class CartaoTestes {
 
     @Autowired
--- proposta/src/test/java/br/zup/proposta/proposta/PropostaApplicationTests.java
+++ proposta/src/test/java/br/zup/proposta/proposta/PropostaApplicationTests.java
@@ -2,8 +2,10 @@ package br.zup.proposta.proposta;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
+@Profile("dev")
 class PropostaApplicationTests {
 
 	@Test

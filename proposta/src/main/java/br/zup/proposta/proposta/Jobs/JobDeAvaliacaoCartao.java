0ae7caedc466ae1bc177494295c012f7c2c3df4b
Kaiofprates <kaioprates@outlook.com>
2021-03-26 01:12:36 -0300

    [feature] melhorando ambiente

--- proposta/src/main/java/br/zup/proposta/proposta/Jobs/JobDeAvaliacaoCartao.java
+++ proposta/src/main/java/br/zup/proposta/proposta/Jobs/JobDeAvaliacaoCartao.java
@@ -31,25 +31,31 @@ public class JobDeAvaliacaoCartao {
 
     @Scheduled(fixedDelayString= "${periodicidade.avalia-cartao}")
     private void avaliaCartoes() {
-        List<Proposta> propostas = repository.findByStatusAndCartao(Estado.ELEGIVEL,null);
-        if(!propostas.isEmpty()){
-            propostas.stream().forEach(this::SaveCartaoId);
+        try{
+            List<Proposta> propostas = repository.findByStatusAndCartao(Estado.ELEGIVEL,null);
+            if(!propostas.isEmpty()){
+                propostas.stream().forEach(this::SaveCartaoId);
+            }
+        }catch (Exception e){
+            logger.info("----------- Aguardando base de dados ------------ ");
         }
+
    }
     public void SaveCartaoId(Proposta proposta){
-        BuscaPropostaResponse response = client.busca(proposta.getId());
+        try{
+            BuscaPropostaResponse response = client.busca(proposta.getId());
+            Cartao cartao = response.toModel();
 
-        Cartao cartao = response.toModel();
-
-        if(cartao != null) {
-            cartaoRepository.save(cartao);
+            if(cartao != null) {
+                cartaoRepository.save(cartao);
+            }
+            proposta.setCartao(response.getId());
+            repository.save(proposta);
+            logger.info("Atualização de cadastro para o id "+ proposta.getId() + " gerada com sucesso");
+        }catch (Exception e){
+            logger.info("Aguardando base de dados ");
         }
 
-        proposta.setCartao(response.getId());
-
-        repository.save(proposta);
-
-        logger.info("Atualização de cadastro para o id "+ proposta.getId() + " gerada com sucesso");
     }
 
 
